962bd9dd8bc9f619384451c294f8f4634407d577
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-05 15:54:53 +0530

    sbi react demo

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Login = () => {
 
     const [loginData, setLoginData] = useState({username: '', password: ''});
+    const [loginMessage, setLoginMessage] = useState('');
 
     const handleLogin = (evt) => {
         console.log(evt.target.name);
@@ -13,13 +14,11 @@ const Login = () => {
         });
     };
 
-    // object destructuring - 
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-
     const submitLogin = (evt) => {
         console.log(loginData);
         alert(`Hi ${loginData.username}! You've logged in successfully!`);
         setLoginData({username: '', password: ''});
+        setLoginMessage(`Hi ${loginData.username}! You've logged in successfully!`);
         evt.preventDefault();
     };
 
@@ -36,11 +35,56 @@ const Login = () => {
                     <input type="submit" value="Login" />
                 </form>
             </div>
+            <p>{loginMessage}</p>
         </div>
     );
 };
 
 export default Login;
+
+// import { useState } from "react";
+
+// const Login = () => {
+
+//     const [loginData, setLoginData] = useState({username: '', password: ''});
+
+//     const handleLogin = (evt) => {
+//         console.log(evt.target.name);
+//         console.log(evt.target.value);
+//         setLoginData({
+//             ...loginData,
+//             [evt.target.name]: evt.target.value
+//         });
+//     };
+
+//     // object destructuring - 
+//     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+//     const submitLogin = (evt) => {
+//         console.log(loginData);
+//         alert(`Hi ${loginData.username}! You've logged in successfully!`);
+//         setLoginData({username: '', password: ''});
+//         evt.preventDefault();
+//     };
+
+//     return (
+//         <div>
+//             <p>Login Component</p>
+//             <div>
+//                 <p>Please login here.</p>
+//                 <form onSubmit={submitLogin}>
+//                     <input type="text" name="username" value={loginData.username} onChange={handleLogin} />
+//                     <br />
+//                     <input type="password" name="password" value={loginData.password} onChange={handleLogin} />
+//                     <br />
+//                     <input type="submit" value="Login" />
+//                 </form>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Login;
 
 // import { useState } from "react";
 
