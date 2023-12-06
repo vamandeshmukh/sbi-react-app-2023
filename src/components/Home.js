bc1477fb2c77440cffcb8cf3974cc6e9df068b25
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-06 14:17:51 +0530

    sbi react demo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
+
     return (
         <div>
-            <p>Home Component</p>
-        </div>
+            <div
+                style={{
+                    backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Samriddhi_Bhavan_-_River_Hooghly_2012-10-23_1265.JPG/2560px-Samriddhi_Bhavan_-_River_Hooghly_2012-10-23_1265.JPG")`,
+                    backgroundRepeat: `no-repeat`,
+                    backgroundSize: 'cover',
+                    backgroundColor: 'black'
+                }}
+            >
+                <div>
+                    <div style={{
+                        minHeight: "100vh",
+                        alignItems: 'center',
+                        justifyContent: 'right',
+                    }} >
+                        <h1 >SBI IT React App</h1>
+                        <Link style={{ textDecoration: 'none' }} to='/emp'>Enter...</Link>
+                    </div>
+                </div>
+            </div >
+        </div >
     );
 };
-
-
-
 export default Home;
+
+
