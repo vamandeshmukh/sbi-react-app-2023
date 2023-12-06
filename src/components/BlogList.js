91d4d152df933b8a44f478497babb38ae81e9be8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-06 15:49:44 +0530

    sbi react demo

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -18,6 +18,7 @@ const BlogList = () => {
         <div>
             <h1>All Blogs List</h1>
             <p> {blogList.length} blogs found. </p>
+            <p>Write youc ode here. </p>
         </div>
     );
 };
