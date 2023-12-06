52889f0f2e44cea49982a60af6d33005fb114940
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-06 15:48:52 +0530

    sbi react demo

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,8 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const BlogList = () => {
+
+    const blogUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+    const [blogList, setBlogList] = useState([]);
+
+    useEffect(() => {
+        axios.get(blogUrl)
+            .then((resp) => {
+                setBlogList(resp.data);
+            })
+    }, []);
 
     return (
         <div>
             <h1>All Blogs List</h1>
+            <p> {blogList.length} blogs found. </p>
         </div>
     );
 };
