dadd228f90fbc252f5838b5edaf2ba54cdb45701
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-06 12:27:18 +0530

    sbi react demo

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import EmpData from './EmpData';
+import Parent from './Parent';
+import Home from './Home';
+import Login from './Login';
+import Logout from './Logout';
 
 const AppRoutes = () => {
 
     return (
         <div>
-            <p>App Routes Component</p>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='home' Component={Home}>Home</Route>
+                    <Route path='emp' Component={EmpData}>Emp</Route>
+                    <Route path='parent' Component={Parent}>Parent</Route>
+                    <Route path='login' Component={Login}>Parent</Route>
+                    <Route path='logout' Component={Logout}>Parent</Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 
